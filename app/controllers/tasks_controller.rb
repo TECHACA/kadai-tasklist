2e3d0bf94d579a0eb2d1b3e1d7773c10e4e5decd
taco.mankichi <taco2nd@gmail.com>
2018-11-22 03:39:29 +0000

    modified task-user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
@@ -14,7 +15,6 @@ class TasksController < ApplicationController
   end
 
   def create 
-    #@task = Task.new(task_params)
     @task = current_user.tasks.build(task_params)
     
     if @task.save
@@ -52,6 +52,13 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+   def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+   end
+ 
   #strong parameter 
   def task_params 
     params.require(:task).permit(:content, :status)
